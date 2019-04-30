2dfcb85f96455b9443384ef71d134e1279057397
Diego Israel Alcantara Salvitano <diegoisrael_a_s@hotmail.com>
2019-04-29 23:30:57 -0500

    Politcas por cada tabla de costos

--- JavaScript/CrearTabla.js
+++ JavaScript/CrearTabla.js
@@ -1,14 +1,39 @@
 // Cada vez que se modifique el tamaño de la tabla
 // ésta se acopla a los argumentos
 $(document).ready(function(){
-    $('table.lectura').html(TablaNM(3, 3));
-    $('input').on('change', function(){
-        var n = $('input').eq(0).val();
-        var m = $('input').eq(1).val();
-        $('table.lectura[name="probabilidades"]').html(TablaNM(n, n));
-        $('table.lectura[name="costos"]').html(TablaNM(n, m));
+    var n = 3;
+    var m = 3;
+    CrearMTablas(n, m);
+    $('input').on('click', function(){
+        n = $('input[name="n"]').val();
+        m = $('input[name="m"]').val();
+        CrearMTablas(n, m);
     });
 });
+
+// Cargar M tablas
+function CrearMTablas(n, m) {
+    var nombres = ['probabilidades', 'costos']
+    var htmlProbCosto = '<table>';
+    for(var j = 1; j <= m; j++){
+        htmlProbCosto += '<tr><table>';
+        htmlProbCosto += '<td><p>' + j + '</p></td>';
+        for(var k = 0; k < 2; k++){
+            htmlProbCosto +=
+              '<td>'
+            + '<p>' + nombres[k] + ':</p>'
+            + '</td>'
+            + '<td>'
+            + '<table class="lectura" name="' + nombres[k] + '">'
+            + (k == 0 ? TablaNM(n, n) : TablaNM(n, m))
+            + '</table>'
+            + '</td>';
+        }
+        htmlProbCosto += '</table></tr>';
+    }
+    htmlProbCosto += '</table>'
+    $('div[name="TablasProbabilidadesCostos"]').html(htmlProbCosto);
+}
 
 // (int, int)
 // Genera el codigo HTML para tener una tabla de NxM
--- JavaScript/GenerarCombinaciones.js
+++ JavaScript/GenerarCombinaciones.js
@@ -1,36 +1,53 @@
 $(document).ready(function(){
-    $('button').click(function(){
-        $('table.politicas').html('');
-        var n = $('input').eq(0).val();
-        var m = $('input').eq(1).val();
-        MostrarPoliticas(n, m);
-    });
+  $('button[name="politicas"]').on('click', function(){
+    var n = parseInt($('input').eq(0).val());
+    var m = parseInt($('input').eq(1).val());
+    var arr = new Array(n);
+
+    CrearTablaPoliticas(m);
+    for(var k = 0; k < m; k++){
+      $('table.politicas').eq(k).html('');
+      GenerarPoliticas(0, n, m, arr, k);
+    }
+  });
 });
 
-function MostrarPoliticas(n, m){
-    var a = new Array(n);
-    GenerarPoliticas(0, n, m, a);
+function CrearTablaPoliticas(m) {
+  htmlTablaPoliticas = '<p>Politicas:</p>';
+  htmlTablaPoliticas += '<table><tr>';
+  for(var it = 0; it < m; it++){
+    num = it + 1;
+    htmlTablaPoliticas += '<td>' + num + '</td>';
+  }
+  htmlTablaPoliticas += '</tr><tr>';
+  for(var it = 0; it < m; it++){
+    htmlTablaPoliticas += '<td><table class="politicas"></table></td>';
+  }
+  htmlTablaPoliticas += '</tr></table>';
+  $('div[name="politicas"]').html(htmlTablaPoliticas);
 }
 
-function GenerarPoliticas(i, n, m, a){
-    if(i == n) {
-        $('table.politicas').append(GenerarPoliticaTr(a));
-        return;
+function GenerarPoliticas(i, n, m, arr, k){
+  if(i == n) {
+    $('table.politicas').eq(k).append(GenerarPoliticaTr(arr));
+    return;
+  }
+  for(var j = 0; j < m; j++){
+    costosTd = $('[name="costos"]').eq(k).find('tr').eq(i);
+    costosTd = costosTd.children().eq(j);
+    if(costosTd.children().eq(0).val() == ''){
+      continue;
     }
-    for(var j = 0; j < m; j++){
-        costosTd = $('table.lectura[name="costos"]').children().eq(i).children().eq(j);
-        if(costosTd.children().eq(0).val() == ''){
-            continue;
-        }
-        a[i] = j;
-        GenerarPoliticas(i + 1, n, m, a);
-    }
+    arr[i] = j;
+    GenerarPoliticas(i + 1, n, m, arr, k);
+  }
 }
 
-function GenerarPoliticaTr(a){
-    htmlRow = '<tr>';
-    for(i in a){
-        htmlRow += '<td>' + a[i] + '</td>';
-    }
-    return htmlRow + '</tr>';
+function GenerarPoliticaTr(arr){
+  htmlRow = '<tr>';
+  for(i in arr){
+    num = arr[i] + 1;
+    htmlRow += '<td>' + num + '</td>';
+  }
+  return htmlRow + '</tr>';
 }
